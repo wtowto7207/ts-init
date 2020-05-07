be80be92a583c7eb7409a8e09332a27ff49c2e79
wtowto7207 <wtowto7207@163.com>
2020-05-07 16:49:20 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <van-button color="#ff0000" size="small" round disabled
+      >vant 按钮</van-button
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import { Component, Vue } from "vue-property-decorator";
+import { Button as VanButton } from "vant";
+
+@Component({
+  components: {
+    HelloWorld,
+    VanButton
+  }
+})
+export default class Home extends Vue {
+  private msg = "";
+}
+</script>
