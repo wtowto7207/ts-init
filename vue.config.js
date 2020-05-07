2b48adfdf05d18d552dd1393ec945741d0f2873c
wtowto7207 <wtowto7207@163.com>
2020-05-07 17:03:15 +0800

    vue.config.js去除ts-loader

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const tsImportPluginFactory = require("ts-import-plugin");
-module.exports = {
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.(jsx|tsx|js|ts)$/,
-          loader: "ts-loader",
-          options: {
-            transpileOnly: true,
-            getCustomTransformers: () => ({
-              before: [
-                tsImportPluginFactory({
-                  libraryName: "vant",
-                  libraryDirectory: "es",
-                  style: true
-                })
-              ]
-            }),
-            compilerOptions: {
-              module: "esnext"
-            }
-          },
-          exclude: /node_modules/
-        }
-      ]
-    }
-  }
-};
+// const tsImportPluginFactory = require("ts-import-plugin");
+// module.exports = {
+//   configureWebpack: {
+//     module: {
+//       rules: [
+//         {
+//           test: /\.(jsx|tsx|js|ts)$/,
+//           loader: "ts-loader",
+//           options: {
+//             transpileOnly: true,
+//             getCustomTransformers: () => ({
+//               before: [
+//                 tsImportPluginFactory({
+//                   libraryName: "vant",
+//                   libraryDirectory: "es",
+//                   style: true
+//                 })
+//               ]
+//             }),
+//             compilerOptions: {
+//               module: "esnext"
+//             }
+//           },
+//           exclude: /node_modules/
+//         }
+//       ]
+//     }
+//   }
+// };
