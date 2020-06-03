fd23ca563b68c8d7487105d1cff450cd106a40b0
wtowto7207 <wtowto7207@163.com>
2020-06-03 21:23:21 +0800

    init redPocket

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <van-button color="#ff0000" size="small" round disabled
-      >vant 按钮</van-button
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <van-button @click="jump">跳转</van-button>
   </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import { Component, Vue } from "vue-property-decorator";
 import { Button as VanButton } from "vant";
 
 @Component({
   components: {
-    HelloWorld,
     VanButton
   }
 })
 export default class Home extends Vue {
   private msg = "";
+  private jump() {
+    this.$router.push({
+      name: "RedPocket"
+    });
+  }
 }
 </script>
