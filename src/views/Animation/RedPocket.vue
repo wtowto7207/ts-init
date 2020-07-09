4ed99d8f21b72bf9a8343f676c40adc10f185f44
duoduo <duoduo@duoduodeMacBook-Pro.local>
2020-07-09 23:56:49 +0800

    添加五角星canvas绘制

--- src/views/Animation/RedPocket.vue
+++ src/views/Animation/RedPocket.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="red-pocket">
-    <canvas ref="redPocket" width="600" height="600">
-      当前浏览器不支持canvas
-    </canvas>
+    <canvas ref="redPocket" width="600" height="600"
+      >当前浏览器不支持canvas</canvas
+    >
   </div>
 </template>
 
@@ -14,6 +14,9 @@ export default class Home extends Vue {
   @Ref() redPocket!: HTMLCanvasElement;
   private initCanvas() {
     const cxt = this.redPocket.getContext("2d") as CanvasRenderingContext2D;
+    return cxt;
+  }
+  private drawChat(cxt: CanvasRenderingContext2D) {
     cxt.fillStyle = "red";
     cxt.beginPath();
     cxt.moveTo(120, 90);
@@ -23,10 +26,59 @@ export default class Home extends Vue {
     cxt.quadraticCurveTo(195, 30, 195, 50);
     cxt.quadraticCurveTo(195, 70, 155, 70);
     cxt.quadraticCurveTo(135, 90, 120, 90);
+    cxt.fillStyle = "#da45ed";
+    cxt.fill();
     cxt.stroke();
   }
+  private drawPentagram(cxt: CanvasRenderingContext2D, rotateAngle: number) {
+    //设置角的数量
+    const angleNum = 5;
+    //设置每个角大小
+    const angle = 360 / angleNum;
+    //设置内外圆半径
+    const outCircleR = 200;
+    const innerCircleR = 80;
+    //设置中心点
+    const x = 200;
+    const y = 200;
+    //计算每个坐标点并开始绘制
+    cxt.beginPath();
+    for (let i = 0; i < angleNum; i++) {
+      cxt.lineTo(
+        Math.cos(((18 - rotateAngle + i * angle) / 180) * Math.PI) *
+          outCircleR +
+          x,
+        -Math.sin(((18 - rotateAngle + i * angle) / 180) * Math.PI) *
+          outCircleR +
+          y
+      );
+      cxt.lineTo(
+        Math.cos(((54 - rotateAngle + i * angle) / 180) * Math.PI) *
+          innerCircleR +
+          x,
+        -Math.sin(((54 - rotateAngle + i * angle) / 180) * Math.PI) *
+          innerCircleR +
+          y
+      );
+    }
+    cxt.closePath();
+    cxt.strokeStyle = "#ff0000";
+    cxt.fillStyle = "#0000ff";
+    cxt.fill();
+    cxt.stroke();
+  }
+  private animatePentagram(cxt: CanvasRenderingContext2D, rotateAngle: number) {
+    cxt.clearRect(0, 0, this.redPocket.width, this.redPocket.height);
+    this.drawPentagram(cxt, rotateAngle);
+  }
   mounted() {
-    this.initCanvas();
+    // const cxt = this.initCanvas();
+    // let rotateAngle = 20;
+    // setInterval(() => {
+    //   rotateAngle += 1;
+    //   this.animatePentagram(cxt, rotateAngle);
+    // }, 17);
+    // console.log(this.redPocket.width);
   }
 }
 </script>
